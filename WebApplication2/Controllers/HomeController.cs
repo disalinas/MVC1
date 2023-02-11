d386d58bab1a46c5c241bd84153525a32749a532
David <davidinaxio@gmail.com>
2023-02-11 23:57:47 +0100

    Agregar archivos de proyecto.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (var context = new Models.SampleContext1())
+            {
+                var aux = context.tipo_vehiculo.ToList();
+            }
+
+            new bbdd.Class1().Ejecutar();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ bbdd/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bbdd
+{
+    public class Class1
+    {
+        public void Ejecutar()
+        {
+            using (var context = new Models.SampleContext2())
+            {
+                var aux = context.tipo_vehiculo.ToList();
+            }
+        }
+    }
+}
